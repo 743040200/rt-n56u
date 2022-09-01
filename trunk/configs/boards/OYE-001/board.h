a56aaef3f9ea5a7dc7e1df18d554cfd52ddd1dad
743040200 <111330893+743040200@users.noreply.github.com>
2022-09-01 14:01:13 +0800

    Update board.h

--- trunk/configs/boards/OYE-001/board.h
+++ trunk/configs/boards/OYE-001/board.h
@@ -2,7 +2,7 @@
 
 #define BOARD_PID		"OYE-001"
 #define BOARD_NAME		"OYE-001"
-#define BOARD_DESC		"OYE-001 Wireless Router"
+#define BOARD_DESC		"OYE-AC Wireless Router"
 #define BOARD_VENDOR_NAME	"OYE"
 #define BOARD_VENDOR_URL	"http://www.oyewifi.com"
 #define BOARD_MODEL_URL		"http://www.oyewifi.com"
